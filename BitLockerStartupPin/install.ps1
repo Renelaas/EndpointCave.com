f213ae626fe01f49bb8853caeb75febf74e5037c
Rene Laas <46025711+Renelaas@users.noreply.github.com>
2022-03-05 16:08:29 +0100

    Upload BitLockerPin Files

---
+++ BitLockerStartupPin/install.ps1
@@ -0,0 +1,2 @@
+New-Item -ItemType Directory -Force -Path C:\progra~2\BitLockerPINDialog\
+Move-Item -Path .\*.* -Destination C:\progra~2\BitLockerPINDialog\
---
+++ BitLockerStartupPin/uninstall.ps1
@@ -0,0 +1,1 @@
+Remove-Item -Path C:\progra~2\BitLockerPINDialog -Recurse
